860bd2ab414dfd623a84fa6ba3d8c8748c6b6cb6
Krisztian Kertesz <kklemming@gmail.com>
2023-09-13 00:25:48 +0200

    feat: output structs

--- formatter-functions.go
+++ formatter-functions.go
@@ -2,16 +2,124 @@ package main
 
 import (
 	"encoding/xml"
+	"fmt"
 	"os"
 )
 
+var test = &Quiz{
+	Questions: []XQuestion{
+		{
+			Type: "matching",
+			Name: Name{
+				Text: Text{
+					Content: "12_35-40 Asszociáció",
+				},
+			},
+			QuestionText: QuestionText{
+				Format: "html",
+				Text: Text{
+					Content: "<![CDATA[<p>Párosítsa az egyes állításokhoz a megfelelő anyagokat!</p>\n<p><strong>   SiO<sub>2</sub></strong><br /><strong>   HCl</strong><br /><strong>   Na</strong><br /><strong>   KBr</strong><br /><strong>   egyik sem</strong><br /></p>]]>",
+				},
+			},
+			GeneralFeedback: GeneralFeedback{
+				Format: "html",
+				Text:   Text{},
+			},
+			DefaultGrade: DefaultGrade{
+				Value: toMoodleFloat(5.0),
+			},
+			Penalty: Penalty{
+				Value: toMoodleFloat(1 / 3),
+			},
+			Hidden: Hidden{
+				Value: 0,
+			},
+			ShuffleAnswers: ShuffleAnswers{
+				Value: false,
+			},
+			CorrectFeedback: CorrectFeedback{
+				Format: "html",
+				Text:   Text{},
+			},
+			PartiallyCorrectFeedback: PartiallyCorrectFeedback{
+				Format: "html",
+				Text:   Text{},
+			},
+			IncorrectFeedback: IncorrectFeedback{
+				Format: "html",
+				Text:   Text{},
+			},
+			SubQuestions: []SubQuestion{
+				{
+					Format: "html",
+					Text: Text{
+						Content: "<![CDATA[<p>atomrácsos kristály</p>]]>",
+					},
+					Answer: Answer{
+						Text: Text{
+							Content: "SiO2",
+						},
+					},
+				},
+				{
+					Format: "html",
+					Text: Text{
+						Content: "<![CDATA[<p>szobahőmérsékleten gáz halmazállapotú</p>]]>",
+					},
+					Answer: Answer{
+						Text: Text{
+							Content: "HCl",
+						},
+					},
+				},
+				{
+					Format: "html",
+					Text: Text{
+						Content: "<![CDATA[<p>puha, szilárd anyag</p>]]>",
+					},
+					Answer: Answer{
+						Text: Text{
+							Content: "Na",
+						},
+					},
+				},
+				{
+					Format: "html",
+					Text: Text{
+						Content: "<![CDATA[<p>szilárd anyag, amely csak olvadt állapotban vezeti az elektromos áramot</p>]]>",
+					},
+					Answer: Answer{
+						Text: Text{
+							Content: "KBr",
+						},
+					},
+				},
+				{
+					Format: "html",
+					Text: Text{
+						Content: "<![CDATA[<p>molekularácsában a részecskéket hidrogén-kötések rögzítik</p>]]>",
+					},
+					Answer: Answer{
+						Text: Text{
+							Content: "egyik sem",
+						},
+					},
+				},
+			},
+		},
+	},
+}
+
 func writeFile(fileName string) {
-	t := &Text{Content: "hi"}
 	header := []byte(xml.Header)
-	out, _ := xml.MarshalIndent(t, " ", "  ")
+	out, _ := xml.MarshalIndent(test, " ", "  ")
 
 	err := os.WriteFile(fileName, append(header, out...), 0644)
 	if err != nil {
 		panic(err)
 	}
+}
+
+func toMoodleFloat(num float32) string {
+	return fmt.Sprintf("%.7f", num)
 }
--- formatter-structs.go
+++ formatter-structs.go
@@ -6,3 +6,91 @@ type Text struct {
 	XMLName xml.Name `xml:"text"`
 	Content string   `xml:",chardata"`
 }
+
+type Name struct {
+	XMLName xml.Name `xml:"name"`
+	Text    Text     `xml:"text"`
+}
+
+type QuestionText struct {
+	XMLName xml.Name `xml:"questiontext"`
+	Format  string   `xml:"format,attr"`
+	Text    Text     `xml:"text"`
+}
+
+type GeneralFeedback struct {
+	XMLName xml.Name `xml:"generalfeedback"`
+	Format  string   `xml:"format,attr"`
+	Text    Text     `xml:"text"`
+}
+
+type DefaultGrade struct {
+	XMLName xml.Name `xml:"defaultgrade"`
+	Value   string   `xml:",chardata"`
+}
+
+type Penalty struct {
+	XMLName xml.Name `xml:"penalty"`
+	Value   string   `xml:",chardata"`
+}
+
+type Hidden struct {
+	XMLName xml.Name `xml:"hidden"`
+	Value   byte     `xml:",chardata"`
+}
+
+type ShuffleAnswers struct {
+	XMLName xml.Name `xml:"shuffleanswers"`
+	Value   bool     `xml:",chardata"`
+}
+
+type CorrectFeedback struct {
+	XMLName xml.Name `xml:"correctfeedback"`
+	Format  string   `xml:"format,attr"`
+	Text    Text     `xml:"text"`
+}
+
+type PartiallyCorrectFeedback struct {
+	XMLName xml.Name `xml:"partiallycorrectfeedback"`
+	Format  string   `xml:"format,attr"`
+	Text    Text     `xml:"text"`
+}
+
+type IncorrectFeedback struct {
+	XMLName xml.Name `xml:"incorrectfeedback"`
+	Format  string   `xml:"format,attr"`
+	Text    Text     `xml:"text"`
+}
+
+type Answer struct {
+	XMLName xml.Name `xml:"answer"`
+	Text    Text     `xml:"text"`
+}
+
+type SubQuestion struct {
+	XMLName xml.Name `xml:"subquestion"`
+	Format  string   `xml:"format,attr"`
+	Text    Text     `xml:"text"`
+	Answer  Answer   `xml:"answer"`
+}
+
+type XQuestion struct {
+	XMLName                  xml.Name                 `xml:"question"`
+	Type                     string                   `xml:"type,attr"`
+	Name                     Name                     `xml:"name"`
+	QuestionText             QuestionText             `xml:"questiontext"`
+	GeneralFeedback          GeneralFeedback          `xml:"generalfeedback"`
+	DefaultGrade             DefaultGrade             `xml:"defaultgrade"`
+	Penalty                  Penalty                  `xml:"penalty"`
+	Hidden                   Hidden                   `xml:"hidden"`
+	ShuffleAnswers           ShuffleAnswers           `xml:"shuffleanswers"`
+	CorrectFeedback          CorrectFeedback          `xml:"correctfeedback"`
+	PartiallyCorrectFeedback PartiallyCorrectFeedback `xml:"partiallycorrectfeedback"`
+	IncorrectFeedback        IncorrectFeedback        `xml:"incorrectfeedback"`
+	SubQuestions             []SubQuestion            `xml:"subquestion"`
+}
+
+type Quiz struct {
+	XMLName   xml.Name    `xml:"quiz"`
+	Questions []XQuestion `xml:"question"`
+}
